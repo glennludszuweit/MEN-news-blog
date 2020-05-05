11e9b00599dd772d9371fdcc29e04bb7a73afd03
glenn <glenn.ludszuweit@gmail.com>
2020-05-05 16:40:31 +0200

    responding to URL params

--- main.js
+++ main.js
@@ -19,6 +19,21 @@ app.get('/api/v1/posts', (req, res) => {
   });
 });
 
+app.get('/api/v1/posts/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  if (id > posts.length) {
+    return res.status(404).json({ status: 'Failed', message: 'Invalid Id' });
+  }
+  const post = posts.find((el) => el.id === id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      post,
+    },
+  });
+});
+
 app.post('/api/v1/posts', (req, res) => {
   const newId = posts[posts.length - 1].id + 1;
   const newPost = Object.assign({ id: newId }, req.body);
