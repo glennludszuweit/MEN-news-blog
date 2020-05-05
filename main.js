2770007786121f3c4812f6e7a48151654bd369fe
glenn <glenn.ludszuweit@gmail.com>
2020-05-05 12:53:09 +0200

    handling GET request from API

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+const posts = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/posts-simple.json`)
+);
+
+app.get('/api/v1/posts', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: posts.length,
+    data: {
+      posts,
+    },
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
