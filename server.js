48b0059090edc7da08c3d6c792093eb60bc12118
glenn <glenn.ludszuweit@gmail.com>
2020-05-06 21:31:10 +0200

    setup .env variables

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env' });
+
 const app = require('./app');
+
+// console.log(process.env);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
