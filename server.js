8b46542f5282c1baf939886fda5bcf7a98ee0ce7
glenn <glenn.ludszuweit@gmail.com>
2020-05-07 00:52:05 +0200

    setup mongodb and mongoose

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app');
 
 dotenv.config({ path: '.env' });
 
-// console.log(process.env);
+mongoose
+  .connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Mongo connection established'));
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
