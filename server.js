0ef504c0e74c3d7d4025eda1a0a01d3376093812
glenn <glenn.ludszuweit@gmail.com>
2020-05-07 01:21:08 +0200

    added sample documents and build a basic post model

--- server.js
+++ server.js
@@ -13,6 +13,49 @@ mongoose
   })
   .then(() => console.log('Mongo connection established'));
 
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Title is needed.'],
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: [true, 'Please add a description for this topic.'],
+  },
+  content: {
+    type: String,
+    required: [true, 'An article must have contents.'],
+  },
+  author: {
+    type: String,
+    required: [true, 'Who is the author?'],
+  },
+  category: {
+    type: String,
+    required: [true, 'Please select category'],
+  },
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+const testPost = new Post({
+  title: 'Node JS',
+  description: 'Node JS tutorial',
+  content: 'The only tutorial you need to master node JS!',
+  author: 'Glenn Ludszuweit',
+  category: 'Technology',
+});
+
+testPost
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
