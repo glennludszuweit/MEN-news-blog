2d9259510db1280d7ebb0ae3fa57d34870e010d0
glenn <glenn.ludszuweit@gmail.com>
2020-05-07 15:45:18 +0200

    delete post with mongoose

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -73,10 +73,18 @@ module.exports = {
     }
   },
 
-  deletePost: (req, res) => {
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
+  deletePost: async (req, res) => {
+    try {
+      await Post.findByIdAndDelete(req.params.id);
+      res.status(204).json({
+        status: 'success',
+        data: null,
+      });
+    } catch (error) {
+      res.status(400).json({
+        status: 'fail',
+        message: error.message,
+      });
+    }
   },
 };
