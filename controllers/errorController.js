91a1bb1dd7d439c100aae8f0a821729e9ad52292
glenn <glenn.ludszuweit@gmail.com>
2020-05-09 16:26:31 +0200

    refactored errorController and added production error delivery

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,14 +1,43 @@
 const AppError = require('../utils/AppError');
 
-module.exports = {
-  globalErrorHanlder: (error, req, res, next) => {
-    console.log(error.stack);
-    error.statusCode = error.statusCode || 500;
-    error.status = error.status || 'error';
+const errorDev = (error, res) => {
+  res.status(error.statusCode).json({
+    status: error.status,
+    error: error,
+    message: error.message,
+    stack: error.stack,
+  });
+};
+
+const errorProd = (error, res) => {
+  //Operational error for client
+  if (error.isOperational) {
     res.status(error.statusCode).json({
       status: error.status,
       message: error.message,
     });
+    //Programming error not for client
+  } else {
+    //log error
+    console.error('ERROR', error);
+    //client generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
+module.exports = {
+  globalErrorHanlder: (error, req, res, next) => {
+    error.statusCode = error.statusCode || 500;
+    error.status = error.status || 'error';
+
+    if (process.env.NODE_ENV === 'development') {
+      errorDev(error, res);
+    } else if (process.env.NODE_ENV === 'production') {
+      errorProd(error, res);
+    }
   },
 
   operationalErrorHandling: (req, res, next) => {
