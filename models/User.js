edd789fafda987cb162f01954fc9e04a57183a95
glenn <glenn.ludszuweit@gmail.com>
2020-05-09 21:26:55 +0200

    created User model

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: validator.isEmail,
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: validator.isAlphanumeric,
+  },
+  confirmPassword: {
+    type: String,
+    required: true,
+    validate: validator.isAlphanumeric,
+  },
+  profileImg: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
