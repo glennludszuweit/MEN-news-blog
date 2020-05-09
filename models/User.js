0110778c1e64ea488f2dbf562c89c828093d5573
glenn <glenn.ludszuweit@gmail.com>
2020-05-09 21:39:34 +0200

    improved User model

--- models/User.js
+++ models/User.js
@@ -4,22 +4,27 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, 'Name required.'],
   },
   email: {
     type: String,
-    required: true,
-    validate: validator.isEmail,
+    required: [true, 'Email is required.'],
+    unique: [true, 'Email already taken'],
+    lowercase: true,
+    validate: [validator.isEmail, 'Email invalid.'],
   },
   password: {
     type: String,
-    required: true,
-    validate: validator.isAlphanumeric,
+    required: [true, 'Passwordm required.'],
+    validate: [
+      validator.isAlphanumeric,
+      'Only alphanumeric is accepted(a-z, 0-9).',
+    ],
+    minlength: [6, 'Password must have atleast 6 characters.'],
   },
   confirmPassword: {
     type: String,
-    required: true,
-    validate: validator.isAlphanumeric,
+    required: [true, 'Please confirm password'],
   },
   profileImg: String,
 });
