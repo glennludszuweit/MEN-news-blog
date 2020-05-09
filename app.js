28a84e52852941ba9334f125f4aeff7016787319
glenn <glenn.ludszuweit@gmail.com>
2020-05-09 12:03:07 +0200

    handled non existing routes

--- app.js
+++ app.js
@@ -23,4 +23,11 @@ app.use((req, res, next) => {
 app.use('/api/v1/posts', postRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl}`,
+  });
+});
+
 module.exports = app;
