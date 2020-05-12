413ee42907cac317c3042703d82b30d665b56dfd
glenn <glenn.ludszuweit@gmail.com>
2020-05-12 13:16:42 +0200

    send JWT via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,18 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOption = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOption.secure = true;
+
+  res.cookie('jwt', token, cookieOption);
+
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
