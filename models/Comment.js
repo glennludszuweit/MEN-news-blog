9479d1a3d7175587951e27f01790e9c6d0bcc4d1
glenn <glenn.ludszuweit@gmail.com>
2020-05-12 19:11:46 +0200

    setup Comment model

---
+++ models/Comment.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema(
+  {
+    comment: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    post: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Post',
+      required: [true, 'Comment must belong to a Post.'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Comment must have a User.'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
--- models/Post.js
+++ models/Post.js
@@ -1,49 +1,53 @@
 const mongoose = require('mongoose');
-const User = require('./User');
 
-const postSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, 'Title is needed.'],
-    unique: true,
-    maxlength: [40, 'Title must have a maximum of 40 characters'],
-    trim: true,
-  },
-  description: {
-    type: String,
-    required: [true, 'Please add a description for this topic.'],
-  },
-  introduction: {
-    type: String,
-    required: [true, 'An article must have introduction.'],
-  },
-  content: {
-    type: String,
-    required: [true, 'An article must have contents.'],
-  },
-  // author: {
-  //   type: String,
-  //   required: [true, 'Who is the author?'],
-  // },
-  author: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
-  category: {
-    type: String,
-    required: [true, 'Please select category.'],
-    enum: {
-      values: ['Politics', 'Technology', 'Entertainment', 'Travel', 'Sport'],
-      message: 'Category not found.',
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Title is needed.'],
+      unique: true,
+      maxlength: [40, 'Title must have a maximum of 40 characters'],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, 'Please add a description for this topic.'],
+    },
+    introduction: {
+      type: String,
+      required: [true, 'An article must have introduction.'],
+    },
+    content: {
+      type: String,
+      required: [true, 'An article must have contents.'],
+    },
+    author: {
+      type: String,
+      required: [true, 'Who is the author?'],
+    },
+    category: {
+      type: String,
+      required: [true, 'Please select category.'],
+      enum: {
+        values: ['Politics', 'Technology', 'Entertainment', 'Travel', 'Sport'],
+        message: 'Category not found.',
+      },
+    },
+    coverImage: {
+      type: String,
+      required: [true, 'A posts must have a cover image.'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
     },
   },
-  coverImage: {
-    type: String,
-    required: [true, 'A posts must have a cover image.'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 const Post = mongoose.model('Post', postSchema);
 
