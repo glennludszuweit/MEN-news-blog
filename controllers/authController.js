1af3bd23cf882bb169ecb42273f54d1c7dc77798
glenn <glenn.ludszuweit@gmail.com>
2020-05-25 15:16:39 +0200

    setup Email class for sending email

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,7 +21,7 @@ const createSendToken = (user, statusCode, res) => {
     httpOnly: true,
   };
 
-  if (process.env.NODE_ENV === 'production') cookieOption.secure = true;
+  if (process.env.NODE_ENV === 'production') cookieOption.secure = false;
 
   res.cookie('jwt', token, cookieOption);
 
--- utils/SendEmail.js
+++ utils/SendEmail.js
@@ -1,28 +1,63 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const HTMLToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.mailtrap.io', //process.env.MAILTRAP_HOST,
-    port: 25, //process.env.MAILTRAP_PORT,
-    secure: false,
-    auth: {
-      user: 'd262be34a8992c', //process.env.MAILTRAP_USER,
-      pass: 'cc5cf3e73fc3cb', //process.env.MAILTRAP_PASS,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
+// new Email(user, url).sendWelcome();
 
-  const mailOptions = {
-    from: '"NewsBlog" <newsblog@email.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html: output,
-  };
+module.exports = class Email {
+  constructor(user, url) {
+    this.from = `NewsBlog <${process.env.EMAIL_FROM}>`;
+    this.to = user.email;
+    this.firsName = user.name.split(' ')[0];
+    this.url = url;
+  }
 
-  await transporter.sendMail(mailOptions);
+  newMailTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //Sendgrid
+      return 1;
+    }
+
+    return nodemailer.createTransport({
+      host: 'smtp.mailtrap.io', //process.env.MAILTRAP_HOST,
+      port: 25, //process.env.MAILTRAP_PORT,
+      secure: false,
+      auth: {
+        user: 'd262be34a8992c', //process.env.MAILTRAP_USER,
+        pass: 'cc5cf3e73fc3cb', //process.env.MAILTRAP_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+  }
+
+  //SEND EMAIL
+  async send(template, subject) {
+    //render html email from pug
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firsName: this.firsName,
+        url: this.url,
+        subject,
+      }
+    );
+
+    //define email option
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: HTMLToText.fromString(html),
+    };
+
+    //create transporter and send the email
+    await this.newMailTransport().transporter.sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to NEWSBLOG!');
+  }
 };
-
-module.exports = sendEmail;
